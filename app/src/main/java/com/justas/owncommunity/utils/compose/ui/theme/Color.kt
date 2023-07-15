1e597223d1e635c7bf64ad7fe6fe091f5f6483f1
Justas Gasparaitis <justas.gasparaitis@telesoftas.com>
2023-07-15 17:05:13 +0300

    Add app colors in Color.kt
    Signed-off-by: Justas Gasparaitis <justas.gasparaitis@telesoftas.com>

--- app/src/main/java/com/justas/owncommunity/utils/compose/ui/theme/Color.kt
+++ app/src/main/java/com/justas/owncommunity/utils/compose/ui/theme/Color.kt
@@ -1,11 +1,16 @@
 package com.justas.owncommunity.utils.compose.ui.theme
 
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val Gradient1 = Color(0xFFF62E8E)
+val Gradient2 = Color(0xFFAC1AF0)
+val GradientBrush = Brush.horizontalGradient(listOf(Gradient1, Gradient2))
+val SocialPink = Color(0xFFF62E8E)
+val SocialBlue = Color(0xFF2E8AF6)
+val DarkBlack = Color(0xFF181A1C)
+val LightWhite = Color(0xFFECEBED)
+val Black = Color(0xFF000000)
+val White = Color(0xFFFFFFFF)
+val Gray = Color(0xFF323436)
+val LightGray = Color(0xFF727477)
