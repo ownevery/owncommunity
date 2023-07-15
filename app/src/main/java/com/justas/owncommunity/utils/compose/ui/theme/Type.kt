2485f60357bc20ed3d330ec4c4bf6b548d768818
Justas Gasparaitis <justas.gasparaitis@telesoftas.com>
2023-07-15 17:05:58 +0300

    Implement typography in Type.kt
    Signed-off-by: Justas Gasparaitis <justas.gasparaitis@telesoftas.com>

--- app/src/main/java/com/justas/owncommunity/utils/compose/ui/theme/Type.kt
+++ app/src/main/java/com/justas/owncommunity/utils/compose/ui/theme/Type.kt
@@ -2,33 +2,44 @@ package com.justas.owncommunity.utils.compose.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.justas.owncommunity.R
 
-// Set of Material typography styles to start with
+val hkGrotesk = FontFamily(
+    Font(R.font.hkgrotesk_medium, FontWeight.Medium),
+    Font(R.font.hkgrotesk_semibold, FontWeight.SemiBold),
+    Font(R.font.hkgrotesk_bold, FontWeight.Bold),
+)
+
+object FontSizes {
+    val tertiary = 12.sp
+    val secondary = 14.sp
+    val body = 16.sp
+    val title = 18.sp
+}
+
 val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
     titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
+        fontFamily = hkGrotesk,
+        fontSize = FontSizes.title,
+        fontWeight = FontWeight.Bold,
     ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
+    bodyLarge = TextStyle(
+        fontFamily = hkGrotesk,
+        fontSize = FontSizes.body,
         fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
-    )
-    */
+    ),
+    bodyMedium = TextStyle(
+        fontFamily = hkGrotesk,
+        fontSize = FontSizes.secondary,
+        fontWeight = FontWeight.Medium,
+    ),
+    bodySmall = TextStyle(
+        fontFamily = hkGrotesk,
+        fontSize = FontSizes.tertiary,
+        fontWeight = FontWeight.Medium,
+    ),
 )
