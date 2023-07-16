918a4d9c814d7da40bb14ba0b6ecfe97435f042b
Justas Gasparaitis <justas.gasparaitis@telesoftas.com>
2023-07-16 17:12:56 +0300

    Add ProfileScreen.kt
    Signed-off-by: Justas Gasparaitis <justas.gasparaitis@telesoftas.com>

---
+++ app/src/main/kotlin/com/gasparaitis/owncommunity/ui/profile/ProfileScreen.kt
@@ -0,0 +1,16 @@
+package com.gasparaitis.owncommunity.ui.profile
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.ramcosta.composedestinations.annotation.Destination
+
+@Destination
+@Composable
+fun ProfileScreen() {
+    Column(modifier = Modifier.fillMaxSize()) {
+        Text("Profile Screen")
+    }
+}
