0dd9793997c2e0164de6be73c47bbf4f4fc5e038
Justas Gasparaitis <justas.gasparaitis@telesoftas.com>
2023-07-16 17:12:24 +0300

    Create `Colors` object in Color.kt
    Signed-off-by: Justas Gasparaitis <justas.gasparaitis@telesoftas.com>

--- app/src/main/kotlin/com/gasparaitis/owncommunity/utils/compose/ui/theme/Color.kt
+++ app/src/main/kotlin/com/gasparaitis/owncommunity/utils/compose/ui/theme/Color.kt
@@ -3,14 +3,17 @@ package com.gasparaitis.owncommunity.utils.compose.ui.theme
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 
-val Gradient1 = Color(0xFFF62E8E)
-val Gradient2 = Color(0xFFAC1AF0)
-val GradientBrush = Brush.horizontalGradient(listOf(Gradient1, Gradient2))
-val SocialPink = Color(0xFFF62E8E)
-val SocialBlue = Color(0xFF2E8AF6)
-val DarkBlack = Color(0xFF181A1C)
-val LightWhite = Color(0xFFECEBED)
-val Black = Color(0xFF000000)
-val White = Color(0xFFFFFFFF)
-val Gray = Color(0xFF323436)
-val LightGray = Color(0xFF727477)
+object Colors {
+    val Transparent = Color(0x00000000)
+    val Gradient1 = Color(0xFFF62E8E)
+    val Gradient2 = Color(0xFFAC1AF0)
+    val GradientBrush = Brush.horizontalGradient(listOf(Gradient1, Gradient2))
+    val SocialPink = Color(0xFFF62E8E)
+    val SocialBlue = Color(0xFF2E8AF6)
+    val DarkBlack = Color(0xFF181A1C)
+    val LightWhite = Color(0xFFECEBED)
+    val PureBlack = Color(0xFF000000)
+    val PureWhite = Color(0xFFFFFFFF)
+    val Gray = Color(0xFF323436)
+    val LightGray = Color(0xFF727477)
+}
