89f274dc892a265343086ac3c85c7151378693b8
Justas Gasparaitis <justas.gasparaitis@telesoftas.com>
2023-07-17 20:15:50 +0300

    Add HomeUiStateDemo.kt
    Signed-off-by: Justas Gasparaitis <justas.gasparaitis@telesoftas.com>

---
+++ app/src/main/kotlin/com/gasparaitis/owncommunity/utils/demo/HomeUiStateDemo.kt
@@ -0,0 +1,77 @@
+// TODO: REMOVE ME
+package com.gasparaitis.owncommunity.utils.demo
+
+import com.gasparaitis.owncommunity.R
+import com.gasparaitis.owncommunity.ui.home.HomeItem
+import com.gasparaitis.owncommunity.ui.home.HomeItemType
+import com.gasparaitis.owncommunity.ui.home.HomeStory
+object HomeUiStateDemo {
+    val post1 = HomeItem(
+        type = HomeItemType.TextOnly,
+        profileImage = R.drawable.demo_profile_1,
+        authorName = "Jacob Washington",
+        postedTimeAgo = "20m ago",
+        bodyText = """“Thers the mind to sleep; to bear, the undiscorns ther in the pation: when we heart-ache undisprises consience the spurns of that we know no troud man's thers this the rub; for in thought himself might, and long a sleep: perchan fly to, 'tis all; and sweat wills be when we hue of action delay, and scove, by opposing afterpriz'd contumely, the question: when we ent with a bare bodkin? Who would by of that is against of the pangs of us for in the naturn awry, to gread office, thought his thus coil, and """.trimMargin(),
+        likeCount = "2,245",
+        commentCount = "45",
+        shareCount = "124",
+        isBookmarked = false,
+        isLiked = true,
+        isShared = true
+    )
+    val post2 = HomeItem(
+        type = HomeItemType.Images(images = listOf(R.drawable.demo_story_2, R.drawable.demo_story_3)),
+        profileImage = R.drawable.demo_profile_2,
+        authorName = "Kat Williams",
+        postedTimeAgo = "1h ago",
+        bodyText = "\"The greatest glory in living lies not in never falling, but in rising every time we fall.\" -Nelson Mandela",
+        likeCount = "8,998",
+        commentCount = "145",
+        shareCount = "12",
+        isBookmarked = false,
+        isShared = true,
+    )
+    val post3 = HomeItem(
+        type = HomeItemType.TextOnly,
+        profileImage = R.drawable.demo_profile_3,
+        authorName = "Tony Monta",
+        postedTimeAgo = "1h ago",
+        bodyText = "Writing code is not so bad!",
+        likeCount = "14",
+        commentCount = "",
+        shareCount = "",
+        isBookmarked = false,
+        isLiked = true,
+    )
+    val post4 = HomeItem(
+        type = HomeItemType.Images(images = listOf(R.drawable.demo_story_1)),
+        profileImage = R.drawable.demo_profile_4,
+        authorName = "Jessica Thompson",
+        postedTimeAgo = "2h ago",
+        bodyText = "\"Go confidently in the direction of your dreams! Live the life you've imagined.\" -Henry David Thoreau",
+        likeCount = "17",
+        commentCount = "2",
+        shareCount = "",
+        isBookmarked = false,
+    )
+    val story1 = HomeStory(
+        storyImage = R.drawable.demo_story_1,
+        profileImage = R.drawable.demo_profile_1,
+        isRead = false,
+    )
+    val story2 = HomeStory(
+        storyImage = R.drawable.demo_story_2,
+        profileImage = R.drawable.demo_profile_2,
+        isRead = true,
+    )
+    val story3 = HomeStory(
+        storyImage = R.drawable.demo_story_3,
+        profileImage = R.drawable.demo_profile_3,
+        isRead = false,
+    )
+    val story4 = HomeStory(
+        storyImage = R.drawable.demo_story_4,
+        profileImage = R.drawable.demo_profile_4,
+        isRead = true,
+    )
+}
