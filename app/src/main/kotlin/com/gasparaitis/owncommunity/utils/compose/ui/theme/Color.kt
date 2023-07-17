856a1862d3b8fd3d82f4cb451e81c789059904a3
Justas Gasparaitis <justas.gasparaitis@telesoftas.com>
2023-07-17 20:11:24 +0300

    Remove value in Color.kt
    Signed-off-by: Justas Gasparaitis <justas.gasparaitis@telesoftas.com>

--- app/src/main/kotlin/com/gasparaitis/owncommunity/utils/compose/ui/theme/Color.kt
+++ app/src/main/kotlin/com/gasparaitis/owncommunity/utils/compose/ui/theme/Color.kt
@@ -1,13 +1,11 @@
 package com.gasparaitis.owncommunity.utils.compose.ui.theme
 
-import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 
 object Colors {
     val Transparent = Color(0x00000000)
     val Gradient1 = Color(0xFFF62E8E)
     val Gradient2 = Color(0xFFAC1AF0)
-    val GradientBrush = Brush.horizontalGradient(listOf(Gradient1, Gradient2))
     val SocialPink = Color(0xFFF62E8E)
     val SocialBlue = Color(0xFF2E8AF6)
     val DarkBlack = Color(0xFF181A1C)
