5f0d0a7d170254d327244a803898c243850ea4b6
Justas Gasparaitis <justas.gasparaitis@telesoftas.com>
2023-07-18 19:00:36 +0300

    Update `EMPTY` in HomeStory.kt
    Signed-off-by: Justas Gasparaitis <justas.gasparaitis@telesoftas.com>

--- app/src/main/kotlin/com/gasparaitis/owncommunity/utils/home/entity/HomeStory.kt
+++ app/src/main/kotlin/com/gasparaitis/owncommunity/utils/home/entity/HomeStory.kt
@@ -1,18 +1,16 @@
 package com.gasparaitis.owncommunity.utils.home.entity
 
 import androidx.annotation.DrawableRes
-import com.gasparaitis.owncommunity.R
-import java.util.UUID
 
 data class HomeStory(
     val id: String,
-    @DrawableRes val storyImage: Int = R.drawable.demo_story_1,
-    @DrawableRes val profileImage: Int = R.drawable.demo_profile_1,
-    val isRead: Boolean = false,
+    @DrawableRes val storyImage: Int,
+    @DrawableRes val profileImage: Int,
+    val isRead: Boolean,
 ) {
     companion object {
         val EMPTY = HomeStory(
-            id = UUID.randomUUID().toString(),
+            id = "",
             storyImage = 0,
             profileImage = 0,
             isRead = false,
